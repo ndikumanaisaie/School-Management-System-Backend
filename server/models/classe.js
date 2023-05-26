57a9ebb8be8ccbf5e8091c2ed938ab678b966a74
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-05-26 19:39:49 +0300

    Add class model

---
+++ server/models/classe.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+// Teacher Modal Schema
+const accountantSchema = new mongoose.Schema({
+  id: { type: String },
+  name: { type: String, required: true },
+	teacher: { type: String, required: false },
+  classTime: {
+		type: Date,
+		default: new Date(),
+	},
+  imageFile: String,
+})
