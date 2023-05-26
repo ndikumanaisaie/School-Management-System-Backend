3ce8e60f934d9095b85c82f44574f2cb90ecf824
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-05-26 17:19:01 +0300

    Add admin and accountant models

---
+++ server/models/admin.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+// Teacher Modal Schema
+const adminSchema = new mongoose.Schema({
+  id: { type: String },
+  name: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: false },
+	googleId: { type: String, required: false },
+  imageFile: String,
+})
