11a0d90286a366da0915b49c9b89699eafcaaa02
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-05-31 13:40:52 +0300

    Add course mark model

---
+++ server/models/Mark.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+// Student Modal Schema
+const markSchema = new mongoose.Schema({
+  homework: { type: Number },
+  quiz: { type: Number, required: true },
+	exam: { type: Number, required: true },
+	total: { type: Number, required: false },
+	parcentage: { type: Number, required: false },
+  course: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Course"
+		}
+	],
+  teacher:[ 
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Teacher"
+		}
+  ],
+  student: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Student"
+		}
+	],
+});
+
+export default mongoose.model('Marks', markSchema);
