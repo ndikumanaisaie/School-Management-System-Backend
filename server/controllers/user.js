ecbdbcfd1831cb6b8fb56366eacf0e5a506aec55
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-06-19 15:37:13 +0200

    Fix extension error

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-import UserModel from '../models/user';
+import UserModel from '../models/user.js';
 
 const secret = 'secret';
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, signin, googleSignIn } from '../controllers/user';
+import { signup, signin, googleSignIn } from '../controllers/user.js';
 
 const router = express.Router();
 
